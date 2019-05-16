5cb028bb3f5d4a717b4e63f5b442edcfefda84f6
SpencerShelton <49757000+SpencerShelton@users.noreply.github.com>
2019-05-16 01:09:58 -0400

    Add files via upload

---
+++ Schedule Creator/Schedule CreatorTests/Schedule_CreatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Schedule_CreatorTests.swift
+//  Schedule CreatorTests
+//
+//  Created by April Shelton on 5/15/19.
+//  Copyright © 2019 Hyberion. All rights reserved.
+//
+
+import XCTest
+@testable import Schedule_Creator
+
+class Schedule_CreatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
